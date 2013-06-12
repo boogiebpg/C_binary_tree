f88c69eb34d16c826c9b2627e823d401841c088e
boogie <booogie@gmail.com>
2013-06-12 11:07:31 +0300

    ... still not working

--- binary_tree.c
+++ binary_tree.c
@@ -4,27 +4,82 @@
 
 struct node
 {
-  void *data;
+  int data;
   struct node *left;
   struct node *right;
 };
 
+int is_empty_tree(struct node *node);
 struct node* tree_init();
+void insert(int data, struct node *node);
+void destroy_tree(struct node *node);
 
-/* initialize tree */
+int main()
+{
+  struct node *root;
+
+  root = tree_init();
+  printf("%d\n",root->data);
+  insert(5,root);
+  printf("and here\n");
+  //printf("%d\n",root->data);
+  insert(7,root);
+  printf("and here\n");
+  printf("%d\n",root->data);
+  destroy_tree(root);
+
+  return 1;
+}
+
+/* check if specified list is empty */
+int is_empty_tree(struct node *node)
+{
+    if (node == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/* initialize node */
 struct node* tree_init()
 {
-    struct node *root;
-
-    root = malloc( sizeof(struct node) );
-    root->data = NULL;
-    root->left = 0;
-    root->right = 0;
+    struct node *root = 0;
 
     return root;
 }
 
-int main()
+void insert(int data, struct node* node)
 {
-	root = tree_init();
+    if( is_empty_tree(node) )
+    {
+        node = malloc( sizeof( node ) );
+        node->data = data;
+        node->left = 0;
+        node->right = 0;
+        printf("we here\n");
+    }
+    else if(data < node->data)
+    {
+        printf("test1\n");
+        insert( data, node->left );
+    }
+    else if(data > node->data)
+    {
+        printf("test2\n");
+        insert( data, node->right );
+    }
+}
+
+void destroy_tree(struct node* node)
+{
+  if ( !is_empty_tree(node) )
+  {
+      destroy_tree(node->left);
+      destroy_tree(node->right);
+      free(node);
+  }
 }
